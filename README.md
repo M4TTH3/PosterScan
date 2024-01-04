c9f9f39682c9905c1df13681d42576b876980159
M4TTH3 <matthewau-yeung@hotmail.com>
2024-01-04 16:51:40 -0500

    finished flask app

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-#Creators:
+# Creators:
 Matthew, Becky, Jerry
 
+## Packages to install for Ubuntu:
+```bash
+sudo apt-get update
+sudo apt-get upgrade
+```
+
+### Install Docker
+Now we install the Docker https://docs.docker.com/engine/install/ubuntu/
+```bash
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+```
+
+```bash
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+```
+
+After Docker is installed, navigate to project's root folder.
+```bash
+sudo docker compose up -d
+```
+
+To Do:
+- Add env variables to docker setup for prod vs dev
+
+### Install libzbar0
+```bash
+sudo apt-get install libzbar0
+```
--- posterscan-backend/qrposterscan.py
+++ posterscan-backend/qrposterscan.py
@@ -1,56 +1,70 @@
 from qreader import QReader
-from requests import request
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import cv2
+import requests as req
+from bs4 import BeautifulSoup as Soup
+from numpy import ndarray
+
 
 class PosterQRScanner:
-
     cur_link: str
     _reader: QReader
 
     def __init__(self) -> None:
         self.cur_link = ""
         self._reader = QReader()
-    def readQR(self, img) -> bool:
+
+    def scan_qr(self, img: "ndarray") -> str:
+        """
+        Scans the QR and returns the text from the website.
+        If no QR or no Text, then it returns ""
+        """
+        if self.read_qr(img):
+            return self.web_info(self.cur_link)
+
+        return ""
+
+    def read_qr(self, img) -> bool:
         """
         Scan the QR
         Returns whether the image produced a link
         """
         # return_detections returns the detection details
         # https://pypi.org/project/qreader/#QReader_detect_table
-        link: str = self._reader.detect_and_decode(image=img)        
+        link: str = self._reader.detect_and_decode(image=img)
         decoded_strings = self._reader.detect_and_decode(image=img)
 
         # Iterate over each decoded string
         for decoded_str in decoded_strings:
-            if decoded_str is not None and 'http' in decoded_str:
+            if decoded_str is not None and "http" in decoded_str:
                 self.cur_link = decoded_str
                 return True
 
         return False
-    
-    def webInfo(self,url)-> str:
-        if (url):
-            page = urlopen(url)
-            html_bytes = page.read()
-            html = html_bytes.decode("utf-8")
-            return html
+
+    def web_info(self, url) -> str:
+        if url:
+            page = req.get(url)
+            html_bytes = page.content
+
+            soup = Soup(html_bytes, "html.parser")
+
+            all_text = soup.get_text(" ", strip=True)
+
+            return all_text
+
         return ""
 
 
-if __name__ == "__main__":
-    scanner = PosterQRScanner()
-    test_images = ["./after-image/youtubee.png", "./after-image/Untitled (30).png"]
-    for img_path in test_images:
-        imgg = cv2.imread(img_path)
-        if imgg is None:
-            print(f"Failed to load image from {img_path}")
-        else:
-            img = cv2.cvtColor(imgg, cv2.COLOR_BGR2RGB)        
-        if scanner.readQR(img):
-            print(f"Found URL: {scanner.cur_link}")
-            print(scanner.webInfo(scanner.cur_link))
-        else:
-            print("No URL found in this image")
-        
+# if __name__ == "__main__":
+#     scanner = PosterQRScanner()
+#     test_images = ["./after-image/youtubee.png", "./after-image/Untitled (30).png"]
+#     for img_path in test_images:
+#         imgg = cv2.imread(img_path, 0)
+#         if imgg is None:
+#             print(f"Failed to load image from {img_path}")
+#         else:
+#             img = cv2.cvtColor(imgg, cv2.COLOR_BGR2RGB)
+#         if scanner.readQR(img):
+#             print(f"Found URL: {scanner.cur_link}")
+#             print(scanner.webInfo(scanner.cur_link))
+#         else:
+#             print("No URL found in this image")

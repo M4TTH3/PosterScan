f514bfea9081ac102812d11b21c1387ee6a3d5dd
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2024-01-04 01:21:25 -0500

    Add frontend

---
+++ posterscan-frontend/posterscan.client/app/page.tsx
@@ -0,0 +1,14 @@
+import Greet from "@/components/Greet";
+import ShowUsersName from "@/components/ShowUsersNames";
+import TestSharp from "@/components/TestSharp"
+
+export default async function Home() {
+
+  return (
+    <>
+      <Greet />
+      <ShowUsersName />
+      <TestSharp />
+    </>
+  )
+}
